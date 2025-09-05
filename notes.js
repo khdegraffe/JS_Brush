19547ca6d2a904f1a0940f43bbac971049f5d59c
khdegraffe <khdegraffe@gmail.com>
2025-09-05 11:51:06 -0400

    add change

--- notes.js
+++ notes.js
@@ -25,16 +25,18 @@ const form = document.querySelector("#shelterForm");
 const input = document.querySelector("#catName");
 const list = document.querySelector("#list");
 
-input.addEventListener("click", () => {});
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const catName = input.value;
-  const newLI = document.createElement("li");
-  newLI.innerText = catName;
-  list.append(newLI);
-  input.value = "";
+input.addEventListener("input", () => {
+  console.log(input.value);
 });
+
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   const catName = input.value;
+//   const newLI = document.createElement("li");
+//   newLI.innerText = catName;
+//   list.append(newLI);
+//   input.value = "";
+// });
 
 // const form = document.querySelector("form");
 // const qtyInput = document.querySelector("#qty");
